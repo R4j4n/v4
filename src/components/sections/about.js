55a22622f4592d81412ec26462d588f24b4f46e5
r4j4n <Rajanghimire534@gmail.com>
2021-05-29 13:47:07 +0545

    navbarandskills

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'React', 'Eleventy', 'Vue', 'Node.js', 'WordPress'];
+  const skills = ['Python', 'C++', 'Tensorflow', 'Keras', 'OpenCv', 'PostgreSQL', 'Pytorch'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
